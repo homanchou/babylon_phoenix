e61d1d6dec9d88703b06073589296e62f7a5d49d
Homan Chou <homan@sellbrite.com>
2020-07-12 23:01:29 -0700

    make changes for heroku

--- lib/babylon_phoenix_web/endpoint.ex
+++ lib/babylon_phoenix_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule BabylonPhoenixWeb.Endpoint do
   ]
 
   socket "/socket", BabylonPhoenixWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
